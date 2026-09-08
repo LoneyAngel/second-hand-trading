9ceb1280e1cc260110c861a36d712e8a63480b3a
Loney Angel <2670696747@qq.com>
2026-09-08 13:07:38 +0800

    url修改

--- f/src/utils/axios.ts
+++ f/src/utils/axios.ts
@@ -27,7 +27,7 @@ interface RetryConfig extends InternalAxiosRequestConfig {
 
 class Request {
   private instance: AxiosInstance;
-  private baseURL = 'https://closes-anytime-wages-personally.trycloudflare.com/api';
+  private baseURL = 'https://cannon-read-rocks-vendor.trycloudflare.com/api';
 
   private timeout = 10000;
   private isRefreshing = false; // 锁：是否正在刷新 Token
